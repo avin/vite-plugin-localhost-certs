ec4ff313ea212931fd56f7a59c5e7ce398f26103
a.vinogradov <a.vinogradov@bpcbt.com>
2022-08-14 23:29:58 +0300

    cjs

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-import fs from 'fs';
+const fs = require('fs');
+const path = require('path');
 
-export default () => {
-  return {
-    name: 'vite-plugin-localhost-certs',
-    enforce: 'pre',
-    config: (config) => {
-      const rootPath = path.dirname(require.resolve('localhost-certs/package.json'));
-      const keyPath = path.resolve(rootPath, 'files', 'server.key');
-      const certPath = path.resolve(rootPath, 'files', 'server.crt');
-      config.server.https = {
-        key: fs.readFileSync(keyPath, 'utf8'),
-        cert: fs.readFileSync(certPath, 'utf8'),
-      };
-    },
-  }
-}
+module.exports = () => ({
+  name: 'vite-plugin-localhost-certs',
+  enforce: 'pre',
+  config: (config) => {
+    const rootPath = path.dirname(require.resolve('localhost-certs/package.json'));
+    const keyPath = path.resolve(rootPath, 'files', 'server.key');
+    const certPath = path.resolve(rootPath, 'files', 'server.crt');
+    config.server.https = {
+      key: fs.readFileSync(keyPath, 'utf8'),
+      cert: fs.readFileSync(certPath, 'utf8'),
+    };
+  },
+});
