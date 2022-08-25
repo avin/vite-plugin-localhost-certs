d9b7064b111ccc522c034527a7bb0840ddc6e7a7
a.vinogradov <a.vinogradov@bpcbt.com>
2022-08-26 00:06:16 +0300

    refactor

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+const localhostCerts = require('localhost-certs');
 
 module.exports = () => ({
   name: 'vite-plugin-localhost-certs',
   enforce: 'pre',
   config: (config) => {
-    const rootPath = path.dirname(require.resolve('localhost-certs/package.json'));
-    const keyPath = path.resolve(rootPath, 'files', 'server.key');
-    const certPath = path.resolve(rootPath, 'files', 'server.crt');
-    config.server.https = {
-      key: fs.readFileSync(keyPath, 'utf8'),
-      cert: fs.readFileSync(certPath, 'utf8'),
-    };
+    config.server.https = localhostCerts();
   },
 });
