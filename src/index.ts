4d2c7999de19672914e4603f20e07efa74992354
a.vinogradov <a.vinogradov@bpcbt.com>
2023-01-16 21:34:39 +0300

    ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import localhostCerts from "localhost-certs";
 
 export default function localhostCertsPlugin() {
   return {
-    name: "vite-plugin-localhost-certs",
-    enforce: "pre",
+    name: "vite-plugin-localhost-certs" as const,
+    enforce: "pre" as const,
     config: (config) => {
       config.server.https = localhostCerts();
     },
